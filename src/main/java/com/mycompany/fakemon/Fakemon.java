be2738661ff1c4a9d89e6b09ce871345410b29de
Bransue-Samaron <bran.jcs@gmail.com>
2024-09-16 00:52:24 -0600

    Mejorando el Codigo x2

--- src/main/java/com/mycompany/fakemon/Fakemon.java
+++ src/main/java/com/mycompany/fakemon/Fakemon.java
@@ -6,6 +6,6 @@ public class Fakemon {
 
     public static void main(String[] args) {
         System.out.println("Bienvenido a tu aventura Pokemon");
-        System.out.println("¿Eres Chico o Chica?");
+        System.out.println("Eres Chico o Chica?");
     }
 }
