1ebcafba4af2149798d7329ffa989a7ac289ea05
Bransue-Samaron <bran.jcs@gmail.com>
2024-09-16 00:29:23 -0600

    Subiendo el Archivo a GitHub

---
+++ src/main/java/com/mycompany/fakemon/Fakemon.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.fakemon;
+
+
+public class Fakemon {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a tu aventura Pokemon");
+    }
+}
